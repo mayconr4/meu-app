9ef4166633e3082ae2c04706f5cae9922e6d70b1
Maycon henrique <mayconhs2007@gmail.com>
2025-09-02 11:26:31 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.texto}>
+        Hello world olá react native 💩💩💩💩💩💩
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#87CEEB",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  texto: {
+    fontSize: 18,
   },
 });
